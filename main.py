a4fdb0bd7e31b7e138b712b00d2d8d230fde8f36
Marc Juchli <mail@marcjuch.li>
2017-12-27 22:16:42 +0100

    Simplified, extended liqui, use ccxt, remove telegram

--- main.py
+++ main.py
@@ -2,103 +2,122 @@
 
 import requests
 from textblob import TextBlob
-from telegram import TelegramBot
 from twitter import Twitter
 import time
+import ccxt
+from coins import coins
 
 symbol_name = {}
 name_symbol = {}
-
+symbol_exchange = {}
 bot = None
 
 def get_coins_bittrex():
-	"""Populate symbol_name and name_symbol dictionaries with markets trading on Bittrex"""
-		
-	endpoint = "https://bittrex.com/api/v1.1/public/getmarkets"
-	try:
-		markets = requests.get(endpoint).json()["result"]
-		for market in markets:
-			symbol = market["MarketCurrency"]
-			name = market["MarketCurrencyLong"].lower()
-			symbol_name[symbol] = name
-			name_symbol[name] = symbol
-		# print(f'Found {len(markets)} markets.')
-	except Exception as e:
-		print(f'Failed to get markets from {endpoint} ({e})')
-	
+    exchange = ccxt.bittrex()
+    markets = exchange.fetch_markets()
+    try:
+        for market in markets:
+            market = market['info']
+            symbol = market["MarketCurrency"]
+            name = market["MarketCurrencyLong"].lower()
+            symbol_name[symbol] = name
+            name_symbol[name] = symbol
+            symbol_exchange[symbol] = 'bittrex'
+        # print(f'Found {len(markets)} markets.')
+    except Exception as e:
+        print(f'Failed to get markets from Bittrex ({e})')
+
+def get_coins_liqui():
+    exchange = ccxt.liqui()
+    markets = exchange.fetch_markets()
+    try:
+        for market in markets:
+            symbol = market['base']
+            try:
+                name = coins[symbol].lower()
+            except Exception as e:
+                # print(f'Failed to match ' + symbol + '. Consider adding to coins.py.')
+                continue
+            symbol_name[symbol] = name
+            name_symbol[name] = symbol
+            symbol_exchange[symbol] = 'liqui'
+        # print(f'Found {len(markets)} markets.')
+    except Exception as e:
+        print(f'Failed to get markets from Liqui ({e})')
 
 def extract_symbols(text):
-	"""Return trading symbols of cryptocurrencies in text in format (symbol, name) e.g. ("BTC", "bitcoin")"""
-	symbols = set()
-	ignore_tags = ["DT"]
-	words = [w[0].lower() for w in TextBlob(text).tags if w[1] not in ignore_tags]
-	for word in words:
-		if word.upper() in symbol_name:
-			symbols.add((word.upper(), symbol_name[word.upper()]))
-			# print(f'Found symbol: {word.upper()}')
-		elif word.lower() in name_symbol:
-			symbols.add((name_symbol[word.lower()], word.lower()))   
-			# print(f'Found symbol: {name_symbol[word]}')
+    """Return trading symbols of cryptocurrencies in text in format (symbol, name) e.g. ("BTC", "bitcoin")"""
+    symbols = set()
+    ignore_tags = ["DT"]
+    words = [w[0].lower() for w in TextBlob(text).tags if w[1] not in ignore_tags]
+    for word in words:
+        if word.upper() in symbol_name:
+            symbols.add((word.upper(), symbol_name[word.upper()]))
+            # print(f'Found symbol: {word.upper()}')
+        elif word.lower() in name_symbol:
+            symbols.add((name_symbol[word.lower()], word.lower()))
+            # print(f'Found symbol: {name_symbol[word]}')
 
-	return symbols
+    return symbols
 
 def get_sentiment_analysis(text, coins):
-	"""Return the sentiment analysis of coins mentioned in text in
-	the form of a dictionary that aggregates the sentiment of
-	sentences that include each of the coins.
-	"""
-	sentiment = {}
-	blob = TextBlob(text)
-	for sentence in blob.sentences:
-		lowercase_words = [x.lower() for x in sentence.words]
-		for coin in coins:
-			if coin[0].lower() in lowercase_words or coin[1].lower() in lowercase_words:
-				try:
-					sentiment[coin] += sentence.sentiment.polarity
-				except:
-					sentiment[coin] = sentence.sentiment.polarity
-	
-	return sentiment, blob.sentiment.polarity
+    """Return the sentiment analysis of coins mentioned in text in
+    the form of a dictionary that aggregates the sentiment of
+    sentences that include each of the coins.
+    """
+    sentiment = {}
+    blob = TextBlob(text)
+    for sentence in blob.sentences:
+        lowercase_words = [x.lower() for x in sentence.words]
+        for coin in coins:
+            if coin[0].lower() in lowercase_words or coin[1].lower() in lowercase_words:
+                try:
+                    sentiment[coin] += sentence.sentiment.polarity
+                except:
+                    sentiment[coin] = sentence.sentiment.polarity
+
+    return sentiment, blob.sentiment.polarity
 
 def get_verdict(sentiment, overall):
-	"""Use the result from get_sentiment_analysis to determine which coins to buy and
-	return an array of coin symbols e.g. ["XVG", "DGB"]"""
-	to_buy = [x for x in sentiment.keys() if sentiment[x] >= 0]
-	if overall >= 0:
-		# Filter out large coins (ideally take out coins in top 10)
-		to_buy = [x for x in to_buy if x[0] not in ["BTC", "LTC", "ETH"]]
-		return to_buy
-	else:
-		return []
+    """Use the result from get_sentiment_analysis to determine which coins to buy and
+    return an array of coin symbols e.g. ["XVG", "DGB"]"""
+    to_buy = [x for x in sentiment.keys() if sentiment[x] >= 0]
+    if overall >= 0:
+        # Filter out large coins (ideally take out coins in top 10)
+        to_buy = [x for x in to_buy if x[0] not in ["BTC", "LTC", "ETH"]]
+        return to_buy
+    else:
+        return []
 
 def analyze(text):
-	"""
-	1. Extract symbols
-	2. Get sentiment analysis
-	3. Determine which coins to buy
-	"""
-	coins = extract_symbols(text)
-	sentiment, overall = get_sentiment_analysis(text, coins)
-	to_buy = get_verdict(sentiment, overall)
+    """
+    1. Extract symbols
+    2. Get sentiment analysis
+    3. Determine which coins to buy
+    """
+    coins = extract_symbols(text)
+    if coins:
+        sentiment, overall = get_sentiment_analysis(text, coins)
+        to_buy = get_verdict(sentiment, overall)
 
-	return to_buy
-
-def telegram_order_callback(coin, amount):
-	print(coin, amount)
+        return to_buy
+    return []
 
 def twitter_tweet_callback(text, user, link):
-	to_buy = analyze(text)
-	
-	if len(to_buy) > 0:
-		bot.notify_tweet(text, user, link, to_buy)
-    
-if __name__ == "__main__":
-	# Populate coins
-	get_coins_bittrex()
-	# Telegram bot
-	bot = TelegramBot(order_callback=telegram_order_callback)
-	# Twitter stream
-	twitter = Twitter(tweet_callback=twitter_tweet_callback)
+    to_buy = analyze(text)
+    if len(to_buy) > 0:
+        for asset in to_buy:
+            symbol = asset[0]
+            exchange = symbol_exchange[symbol]
+            print('Buy ' + str(symbol) + ' on ' + exchange + '.')
+        #bot.notify_tweet(text, user, link, to_buy)
 
-	while True:
-		time.sleep(1)
+if __name__ == "__main__":
+    # Populate coins
+    get_coins_bittrex()
+    get_coins_liqui()
+    #print(symbol_name)
+    #print(name_symbol)
+
+    # Twitter stream
+    twitter = Twitter(setup=['stream'], tweet_callback=twitter_tweet_callback)
--- twitter.py
+++ twitter.py
@@ -1,44 +1,113 @@
+import io
+from PIL import Image
+import pytesseract
+import requests
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 import json
+from tweepy import Cursor
+from tweepy import API
+
 
 with open("config.json") as f:
-	config = json.load(f)
-	FOLLOW_IDS = config["TWITTER_FOLLOW_IDS"]
+    config = json.load(f)
+    FOLLOW_IDS = config["TWITTER_FOLLOW_IDS"]
+    CONSUMER_KEY = config["TWITTER_CONSUMER_KEY"]
+    CONSUMER_SECRET = config["TWITTER_CONSUMER_SECRET"]
+    ACCESS_KEY = config["TWITTER_ACCESS_KEY"]
+    ACCESS_SECRET = config["TWITTER_ACCESS_SECRET"]
 
-with open("secrets.json") as f:
-	keys = json.load(f) 
-	CONSUMER_KEY = keys["TWITTER_CONSUMER_KEY"]
-	CONSUMER_SECRET = keys["TWITTER_CONSUMER_SECRET"]
-	ACCESS_KEY = keys["TWITTER_ACCESS_KEY"]
-	ACCESS_SECRET = keys["TWITTER_ACCESS_SECRET"]
 
 class TwitterListener(StreamListener):
-	def __init__(self, callback):
-		self.callback = callback
 
-	def on_data(self, data):
-		tweet_json = json.loads(data)
-		if tweet_json["user"]["id_str"] in FOLLOW_IDS:
-			self.callback(tweet_json)
-		
+    def __init__(self, callback):
+        self.callback = callback
+
+    def on_data(self, data):
+        tweet_json = json.loads(data)
+        try:
+            if tweet_json["user"]["id_str"] in FOLLOW_IDS:
+                print(tweet_json["text"])
+                self.callback(tweet_json)
+        except:
+            pass
+
+class CursorListener:
+    def __init__(self, auth, callback):
+        self.auth = auth
+        self.callback = callback
+        self.api = API(self.auth)
+
+    def run(self):
+        for uid in FOLLOW_IDS:
+            cursor = Cursor(self.api.user_timeline, user_id=uid)
+            for status in cursor.items(100):
+                # print(status._json)
+                self.callback(status._json)
+
+
 class Twitter:
-	def __init__(self, tweet_callback=lambda x, y, z: x):
-		self.tweet_callback = tweet_callback
-		
-		self.listener = TwitterListener(self.handle_tweet)
-		
-		self.auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-		self.auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    def __init__(self, setup=['stream'], tweet_callback=lambda x, y, z: x, image_only=False):
+        self.tweet_callback = tweet_callback
+        self.image_only = image_only
 
-		self.stream = Stream(self.auth, self.listener)	
-		self.stream.filter(follow=FOLLOW_IDS)
-	
-	def handle_tweet(self, tweet_json):
-		screen_name = tweet_json["user"]["screen_name"]
-		id = tweet_json["id_str"]
-		text = tweet_json["text"]
-		link = f'https://twitter.com/{screen_name}/status/{id}'
-		print(tweet_json)
-		self.tweet_callback(text, screen_name, link)
+        self.listener = TwitterListener(self.handle_tweet)
+
+        self.auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        self.auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+        if 'stream' in setup:
+            print('Starting to listen to twitter stream...')
+            self.stream = None
+            self.run_stream()
+
+        if 'cursor' in setup:
+            print('Starting to scan twitter timeline...')
+            self.cursor = CursorListener(self.auth, self.handle_tweet)
+            self.cursor.run()
+
+    def run_stream(self):
+        while True:
+            self.stream = Stream(self.auth, self.listener, timeout=60)
+            self.stream.filter(follow=FOLLOW_IDS)
+            try:
+                self.stream.userstream()
+            except Exception as e:
+                print("Error. Restarting Stream.... Error: ")
+                print(e.__doc__)
+                print(e.message)
+
+    def handle_tweet(self, tweet_json):
+        screen_name = tweet_json["user"]["screen_name"]
+        id = tweet_json["id_str"]
+        text = tweet_json["text"].replace("\\", "")
+        is_image = False
+        # Get media if present
+        try:
+            urls = [x["media_url"].replace("\\", "") for x in tweet_json["entities"]["media"] if x["type"] == "photo"]
+            for url in urls:
+                response = requests.get(url)
+                img = Image.open(io.BytesIO(response.content))
+                # Extract text from image
+                # print(img)
+                is_image = True
+                img_text = pytesseract.image_to_string(img)
+                text += f' . {img_text}'
+        except KeyError:
+            pass
+
+        link = f'https://twitter.com/{screen_name}/status/{id}'
+
+        try:
+            if not self.image_only or (self.image_only and is_image):
+                self.tweet_callback(text, screen_name, link)
+        except:
+            pass
+
+if __name__ == "__main__":
+    import time
+    twitter = Twitter()
+    print('Twitter done.')
+    while True:
+        time.sleep(1)
